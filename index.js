92eb781c227d2ffee767ac0aaa45fc2e28b7cc3b
oluyomi justina <justinaolamide2611@gmail.com>
2023-05-09 09:53:44 +0100

    exp

---
+++ index.js
@@ -0,0 +1,21 @@
+function output(event ){
+    event.preventDefault();
+const vOne = document.getElementById
+("vOne").value;
+const vTwo = document.getElementById
+("vTwo").value;
+
+let result = vOne ** vTwo;
+console.log(result)
+}
+
+function outpush(event ){
+    event.preventDefault();
+const nOne = document.getElementById
+("nOne").value;
+const nTwo = document.getElementById
+("nTwo").value;
+
+let outcome = nOne + nTwo;
+console.log(outcome)
+}
